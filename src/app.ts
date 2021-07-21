05fd8bc048468fab6ac0c705dd25cd70a6afdbb5
lucasfranchini <lucasfranchini.a@gmail.com>
2021-07-21 20:36:06 -0300

    start environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
+import connection from "./database";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/test", (req, res) => {
-  res.send("OK!");
+app.get("/test", async (req, res) => {
+  const result = await connection.query('select * from recommendations')
+
+  res.send(result.rows)
 });
 
 export default app;
