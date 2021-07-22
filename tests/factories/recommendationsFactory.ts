43c117b7a5b9a6f0272209bbeb4ee41e4fe28c47
lucasfranchini <lucasfranchini.a@gmail.com>
2021-07-22 02:36:58 -0300

    start test for post Recommendations, make createRecommendations factory

---
+++ tests/factories/recommendationsFactory.ts
@@ -0,0 +1,18 @@
+import faker from "faker";
+import connection from "../../src/database";
+
+export async function CreateRecommendation(){
+    const data = {
+        name: faker.name.title(),
+        link: "https://www.youtube.com/watch?v=oX7kvYrL6lQ"
+    }
+
+    const result = await connection.query(`
+    INSERT INTO recommendations
+    (name,link)
+    VALUES ($1,$2)
+    RETURNING *
+    `,[data.name,data.link])
+
+    return result.rows[0];
+}
--- tests/integration/recommendations.test.ts
+++ tests/integration/recommendations.test.ts
@@ -3,9 +3,13 @@ import supertest from "supertest";
 import app from "../../src/app";
 import { clearDatabase, closeConnection } from "../utils/database";
 
-beforeAll(clearDatabase)
+beforeEach(clearDatabase)
 afterAll(closeConnection)
 
 describe("POST /recommendations", () => {
-  
+  it("returns status 400 from invalid name", async ()=>{})
+  it("returns status 400 from invalid youtube link", async ()=>{})
+  it("returns status 400 from empty params", async ()=>{})
+  it("returns status 409 from duplicated youtube link", async ()=>{})
+  it("returns status 201 from valid params", async ()=>{})
 });
