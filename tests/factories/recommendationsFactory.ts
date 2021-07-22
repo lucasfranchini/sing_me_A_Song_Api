76b7f5c2365d7903cd93c3d5f50914f6d4330ca3
lucasfranchini <lucasfranchini.a@gmail.com>
2021-07-22 12:26:47 -0300

    tests for upvote and downvote

--- tests/factories/recommendationsFactory.ts
+++ tests/factories/recommendationsFactory.ts
@@ -1,11 +1,20 @@
 import faker from "faker";
 import connection from "../../src/database";
 
-export async function generateRecommendation( name:string, link:string ){
+export function generateRecommendation( name:string, link:string ){
     const data = {
         name: name || faker.name.title(),
         youtubeLink: link || "https://www.youtube.com/watch?v=P7jwLxkN8gI"
     }
-
     return data;
+}
+
+export async function saveRecommendationInDatabase(){
+    const data = generateRecommendation('','');
+    await connection.query(`
+    INSERT 
+    INTO recommendations 
+    (name,"youtubeLink") 
+    VALUES ($1,$2)
+    `,[data.name,data.youtubeLink]);
 }
--- tests/integration/recommendations.test.ts
+++ tests/integration/recommendations.test.ts
@@ -2,7 +2,8 @@ import "../../src/setup";
 import supertest from "supertest";
 import app from "../../src/app";
 import { clearDatabase, closeConnection } from "../utils/database";
-import { generateRecommendation } from "../factories/recommendationsFactory";
+import { generateRecommendation, saveRecommendationInDatabase } from "../factories/recommendationsFactory";
+import connection from "../../src/database";
 
 const agent = supertest(app);
 
@@ -10,9 +11,8 @@ beforeEach(clearDatabase)
 afterAll(closeConnection)
 
 describe("POST /recommendations", () => {
-  
   it("returns status 400 from invalid youtube link", async ()=>{
-    const body = await generateRecommendation('','errado');
+    const body = generateRecommendation('','errado');
     const res = await agent.post('/recommendations').send(body);
     expect(res.status).toEqual(400);
   })
@@ -22,15 +22,57 @@ describe("POST /recommendations", () => {
     expect(res.status).toEqual(400);
   })
   it("returns status 201 from valid params", async ()=>{
-    const body = await generateRecommendation('','');
+    const body = generateRecommendation('','');
     const res = await agent.post('/recommendations').send(body);
     expect(res.status).toEqual(201);
   })
   it("returns status 409 from duplicated youtube link", async ()=>{
-    const body = await generateRecommendation('','');
+    const body = generateRecommendation('','');
     await agent.post('/recommendations').send(body);
     const res = await agent.post('/recommendations').send(body);
     expect(res.status).toEqual(409);
   })
-  
 });
+
+describe('POST recommendations/:id/upvote', () =>{
+  beforeEach(saveRecommendationInDatabase)
+  it('returns status 404 for invalid id', async ()=>{
+    const res = await agent.post('/recommendations/3/upvote');
+    expect(res.status).toEqual(404);
+  })
+  it('returns status 200 for valid id', async ()=>{
+    const res = await agent.post('/recommendations/1/upvote');
+    expect(res.status).toEqual(200);
+  })
+  it('adds score for a valid recommendation',async ()=>{
+    await agent.post('/recommendations/1/upvote');
+    const res =  await connection.query('SELECT score FROM recommendations')
+    expect(res.rows[0].score).toEqual(1)
+  })
+})
+
+describe('POST recommendations/:id/downvote', () =>{
+  async function alterScore(){
+    await connection.query("UPDATE recommendations SET score = -5")
+  }
+  beforeEach(saveRecommendationInDatabase)
+  it('returns status 404 for invalid id', async ()=>{
+    const res = await agent.post('/recommendations/3/downvote');
+    expect(res.status).toEqual(404);
+  })
+  it('returns status 200 for valid id', async ()=>{
+    const res = await agent.post('/recommendations/1/downvote');
+    expect(res.status).toEqual(200);
+  })
+  it('subtracts score for a valid recommendation',async ()=>{
+    await agent.post('/recommendations/1/downvote');
+    const res =  await connection.query('SELECT score FROM recommendations')
+    expect(res.rows[0].score).toEqual(-1)
+  })
+  it('removes a recommendations when score is less than -5', async ()=>{
+    await alterScore();
+    await agent.post('/recommendations/1/downvote');
+    const res =  await connection.query('SELECT score FROM recommendations');
+    expect(res.rowCount).toEqual(0);
+  })
+})
