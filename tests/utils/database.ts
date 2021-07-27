2ddd92e1a6c233f81af68a80906609ba4f2afa5c
ronald <ronaldjunior2@hotmail.com>
2021-07-27 16:49:02 -0300

    preparando review

--- tests/utils/database.ts
+++
@@ -1,9 +0,0 @@
-import connection from "../../src/database";
-
-export async function clearDatabase(){
-    await connection.query('TRUNCATE recommendations RESTART IDENTITY')
-}
-
-export async function closeConnection () {
-    await connection.end();
-  }
